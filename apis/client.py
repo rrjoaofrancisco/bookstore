3a9cb19eaed853efcf1fa002ff1470f16b53f849
kikorodrigues <rrjoaofrancisco@gmail.com>
2021-03-26 22:09:18 -0300

    applying fees

--- apis/client.py
+++ apis/client.py
@@ -20,21 +20,28 @@ class Lending(Resource):
         self.service = LendingService()
         self.book_service = BookService()
 
-    def _apply_fees(self, loan_json, today, dt_devolution):
+    def _apply_fees(self, loan_json, today, dt_devolution, value):
         fine = 0.0
         fee = 0.0
+        current_value = value
         if today > dt_devolution:
-            if today - dt_devolution <= timedelta(days=3):
+            date_diff = today - dt_devolution
+            days = date_diff.days
+            if date_diff < timedelta(days=4):
                 fine = 3.0
                 fee = 0.2
-            elif today - dt_devolution <= timedelta(days=5):
+            elif date_diff < timedelta(days=6):
                 fine = 5.0
                 fee = 0.4
             else:
                 fine = 7.0
                 fee = 0.6
+            fee_value = ((days * fee) * value) / 100
+            fine_value = (fine * value) / 100
+            current_value = value + fee_value + fine_value
         loan_json['fine'] = fine
         loan_json['fee'] = fee
+        loan_json['current_value'] = current_value
 
     def get(self, id):
         lendings = self.service.get_by_client(id)
@@ -42,6 +49,6 @@ class Lending(Resource):
         response = []
         for loan in lendings:
             loan_json = lending_schema.dump(loan)
-            self._apply_fees(loan_json,today,loan.devolution_date)
+            self._apply_fees(loan_json,today,loan.devolution_date, loan.value)
             response.append(loan_json)
         return response
