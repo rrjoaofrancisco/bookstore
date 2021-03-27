d9973189062ca2c7c7dbba176a3a2cf02941d16c
kikorodrigues <rrjoaofrancisco@gmail.com>
2021-03-27 16:09:20 -0300

    db pre load

---
+++ db-init-load.sql
@@ -0,0 +1,19 @@
+-- SQLite
+-- DELETE from lending_books
+-- DELETE from book
+-- DELETE from client
+
+INSERT INTO book (id, title, avaiable)
+VALUES (1, 'xunda book', 1);
+INSERT INTO book (id, title, avaiable)
+VALUES (2, 'Livro dos espíritos', 1);
+INSERT INTO book (id, title, avaiable)
+VALUES (3, 'Um lugar ao sol', 1);
+
+INSERT INTO client (id, name)
+VALUES (1,'Julia');
+INSERT INTO client (id, name)
+VALUES (2,'Ana');
+INSERT INTO client (id, name)
+VALUES (3,'Alícia');
+
